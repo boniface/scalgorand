1b2f2e87f1ed0ce46fd01b73ed159ec47bf2c76c
hashcode <boniface@kabaso.com>
2022-07-30 20:58:38 +0200

    Added a Test Container for Algorand Sandbox

--- core/shared/src/main/scala/scalgorand/infrastracture/algod/general/impl/GeneralAPIImpl.scala
+++ core/shared/src/main/scala/scalgorand/infrastracture/algod/general/impl/GeneralAPIImpl.scala
@@ -1,18 +1,27 @@
 package scalgorand.infrastracture.algod.general.impl
 
 import scalgorand.domain.genesis.Genesis
-import scalgorand.infrastracture.InfrastructureError
+import scalgorand.infrastracture.InfrastructureError.GeneralAPIError
 import scalgorand.infrastracture.algod.general.GeneralAPI
 import sttp.client3._
 import sttp.client3.ziojson.asJson
 import zio._
 
 case class GeneralAPIImpl(sttpBackend: SttpBackend[Task, Any]) extends GeneralAPI {
-  override def getGenesis: IO[Option[InfrastructureError], Genesis] = {
+  override def getGenesis: ZIO[Any, Option[GeneralAPIError], Genesis] = {
     val request = basicRequest
       .response(asJson[Genesis])
       .get(uri"http://localhost:4001/genesis")
-    val result  = sttpBackend.send(request)
+    val result  = sttpBackend
+      .send(request)
+      .mapBoth(
+        error => GeneralAPIError(error),
+        result => result.body,
+      )
+    result.flatMap {
+      case Left(value)  => ZIO.none
+      case Right(value) => ZIO.some(value)
+    }.some
   }
 
   override def getHealth: Unit = ???
@@ -23,6 +32,6 @@ case class GeneralAPIImpl(sttpBackend: SttpBackend[Task, Any]) extends GeneralAP
 }
 
 object GeneralAPIImpl {
-  val live: ZLayer[SttpBackend[Task, Any], Any, GeneralAPIImpl] =
+  val layer: ZLayer[SttpBackend[Task, Any], Any, GeneralAPIImpl] =
     ZLayer.fromFunction(new GeneralAPIImpl(_))
 }
